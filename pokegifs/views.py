7700e39cb198e9bedcefab99449c0a0414d0c3dd
jaybenaim <benaimjacob@gmail.com>
2019-08-14 19:25:45 -0400

    check for ValueError for pokemon error and NameError for InvalidKey

--- pokegifs/views.py
+++ pokegifs/views.py
@@ -62,9 +62,7 @@ def login_view(request):
             
             username = form.cleaned_data['username']
             pw = form.cleaned_data['password']
-            
             user = authenticate(username=username, password=pw)
-            
             if user is not None:
                 login(request, user)
                 return HttpResponseRedirect('/home')
