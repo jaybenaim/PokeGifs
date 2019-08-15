c380fcfa181b840ce448d66341631996a8d8d422
jaybenaim <benaimjacob@gmail.com>
2019-08-15 12:34:29 -0400

    added view for team but need to render list of 6 pokemon

--- pokegifs/views.py
+++ pokegifs/views.py
@@ -145,6 +145,42 @@ def login_view(request):
         'form': form
     }) 
 
+# def pokemon_team(request):
+#     try: 
+#         rand = random.randint(1, 100)
+#         api_url = f"http://pokeapi.co/api/v2/pokemon/{rand}/"
+#         res = requests.get(api_url) 
+#         body = json.loads(res.content) 
+#         poke_name = body['name']
+#         random_pokemon = random.randint(1, 100)
+#         poke_id = random_pokemon
+#         poke_type = body['types'][0]['type']['name']
+
+#         giphy_api_key = os.environ.get('GIPHY_API_KEY')
+#         query = poke_name
+#         limit = 20
+#         offset = 0
+#         rating = 'G'   
+#         giphy_url = f"https://api.giphy.com/v1/gifs/search?api_key={giphy_api_key}&q={query}&limit={int(limit)}&offset={int(offset)}&rating={rating.upper()}&lang=en"
+
+#         giphy_res = requests.get(giphy_url)
+#         giphy_body = json.loads(giphy_res.content) 
+        
+#         # data = giphy_res.json()
+#         data = giphy_body['data'][0]
+#         gif = data['images']['fixed_height']['url'] 
+        
+        
+#     except ValueError: 
+#         random_error_page = random.randint(1, 100)
+#         return redirect(reverse('pokemon_show', args=[random_error_page]))
+#     except NameError: 
+#         random_error_page = random.randint(1, 100)
+#         return redirect(reverse('pokemon_show', args=[random_error_page]))
+    
+#     return redirect(reverse('pokemon_show', args=[poke_id]))
+
+    
 
 def signup(request):
     form = UserCreationForm() 
